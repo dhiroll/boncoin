a7208637df4fc38799ca506b46d728c125733805
Dhivya ROLLIN <dhivyarollin@MacBook-Pro-de-Dhivya.local>
2018-02-05 17:28:37 +0100

    create pages

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+var multer = require('multer');
 var upload = multer({ dest: "public/uploads/" });
 
 app.use(express.static('public'));
@@ -11,7 +12,7 @@ var _ = require('lodash');
 // Load the core build.
 var _ = require('lodash/core');
 
-var students = [];
+var announces = [];
 var idCounter = 0;
 
 app.get('/', function (req, res) {
@@ -24,23 +25,31 @@ app.get('/deposer', function (req, res) {
 
 app.post('/deposer', upload.single('photo_url'), function (req, res) {
 
-  var first_name = req.body.first_name;
-  var biography = req.body.biography;
+ 
+
+  var title_name = req.body.title_name;
+  var photo_url = req.file.filename;
+  var nick_name = req.body.nick_name;
+  var price = req.body.price;
   var city = req.body.city;
-
-  var photo_url = req.file.filename; // uploads/493b27cc2bb406686c4dd3e4dbd90b7d
-
-  var newStudent = {
+  var description = req.body.description;
+  var phone_number = req.body.phone_number;
+ 
+  var newAnnounce = {
     id: idCounter,
-    first_name: first_name,
-    biography: biography,
-    city: city,
+    title_name : title_name,
     photo_url: photo_url,
+    nick_name: nick_name,
+    price: price,
+    city: city,
+    description: description,
+    phone_number: phone_number
+  
   };
 
   idCounter++;
 
-  students.push(newStudent);
+  announces.push(newAnnounce);
 
   // Push the new profile to the students array
   res.redirect('/annonce/' + newAnnounce.id);
@@ -49,15 +58,19 @@ app.post('/deposer', upload.single('photo_url'), function (req, res) {
 app.get('/annonce/:id', function (req, res) {
 
   var id = parseInt(req.params.id);
-
-  for (var i = 0; i < students.length; i++) {
-    if (students[i].id === id) {
-      return res.render('viewAnnounce.ejs', {
-        id: students[i].id,
-        first_name: students[i].first_name,
-        biography: students[i].biography,
-        city: students[i].city,
-        photo_url: students[i].photo_url,
+s
+  for (var i = 0; i < announces.length; i++) {
+    if (announces[i].id === id) {
+      return res.render('viewAnnounce.ejs', {  
+        id: announces[i].id,
+        title_name: announces[i].title_name,
+        photo_url: announces[i].photo_url,
+        nick_name: announces[i].nick_name,
+        price: announces[i].price,
+        city: announces[i].city,
+        description: announces[i].description,
+        phone_number:announces[i].phone_number
+       
       });
     }
   }
